ee79cda5f17cf23a4f5d469653684c18b3bda318
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-09-03 12:59:09 +0200

    feat: Update print format in 5-print_comb2.py
    This commit updates the print format in the "5-print_comb2.py" script to use the format method instead of f-strings. This change improves the readability and maintainability of the code. It is part of the "python-if_else_loops_functions" project.

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for i in range(0, 100):
     if i < 99:
-        print(f"{i:02d}, ", end="")
+        print("{:02d}, ".format(i), end="")
     else:
-        print("99")
+        print("{:02d}".format(i))
