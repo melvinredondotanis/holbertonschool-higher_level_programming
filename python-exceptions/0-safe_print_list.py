0493a9cc7e14b2ea52c961c7e5f9a079aaea1b70
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-09-11 14:32:12 +0200

    refactor: Update safe_print_list function to return the correct index
    Update the safe_print_list function to return the correct index by adding 1 to the value returned in case of successful printing. This ensures that the index returned matches the number of elements printed.
    
    refactor: Update safe_print_integer function to handle TypeError
    
    Modify the safe_print_integer function to handle both ValueError and TypeError exceptions. This allows the function to gracefully handle cases where the input value is not an integer.

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
         print()
-        return i
+        return i + 1
     except IndexError:
         print()
         return i
