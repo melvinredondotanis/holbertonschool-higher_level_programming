ba167ff7954f289a538767cfafdeed157d02de7b
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-09-17 14:52:43 +0200

    Refactoriser les classes Square pour utiliser une variable d'instance pour la taille

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,4 +7,19 @@ class Square:
     This is an empty class Square that defines a square.
     """
     def __init__(self, size):
+        """
+        The __init__ method of the class Square
+        initializes the instance.
+
+        Args:
+            size (int): The size of the square
+
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+
+        Returns:
+            None
+        """
+
         self._size = size
