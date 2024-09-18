3e0b0c34bd9c0065d7761a5ca29ac676c1e14e4d
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-09-18 11:26:29 +0200

    Refactoriser les classes Square pour utiliser une variable d'instance privée pour la taille

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -82,15 +82,3 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)
