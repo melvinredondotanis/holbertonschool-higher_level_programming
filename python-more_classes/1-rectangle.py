81679b9ad3221db1cf1decc79216fef79f2fbe7c
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-09-19 12:30:20 +0200

    Refactoriser la classe Rectangle pour supprimer la méthode __dict__

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -3,16 +3,20 @@
 
 
 class Rectangle:
+    """Class that defines a rectangle"""
     def __init__(self, width=0, height=0):
+        """Initializes the rectangle"""
         self.height = height
         self.width = width
 
     @property
     def width(self):
+        """Returns the width of the rectangle"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Sets the width of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -21,10 +25,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Returns the height of the rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Sets the height of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
