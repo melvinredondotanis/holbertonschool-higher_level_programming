e7f0f891d217649a4adee5b4385737e7fd631bc7
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-09-19 11:38:06 +0200

    Refactoriser les classes Rectangle pour utiliser des variables d'instance privées pour la largeur et la hauteur

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that defines a class Rectangle"""
+
+
+class Rectangle:
+    """Class that defines a rectangle"""
+
+    def __dict__(self):
+        """
+        Method that returns the dictionary
+        representation of a Rectangle.
+        """
+
+        return self.__dict__
