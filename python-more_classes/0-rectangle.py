32c7a47602490497555d48de277337edc550be5c
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-09-19 12:27:32 +0200

    Refactoriser la classe Rectangle pour supprimer la méthode __dict__

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -4,11 +4,4 @@
 
 class Rectangle:
     """Class that defines a rectangle"""
-
-    def __dict__(self):
-        """
-        Method that returns the dictionary
-        representation of a Rectangle.
-        """
-
-        return self.__dict__
+    pass
