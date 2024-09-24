e30cf205e2b1f64739796463c2de5954a36ec97c
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-09-24 16:33:59 +0200

    Refactoriser la classe BaseGeometry pour supprimer la méthode area() non implémentée

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that defines a base geometry class"""
+
+
+class BaseGeometry:
+    """Defines methods for geometric shapes."""
+    
+    def area(self):
+        """Calculate the area of a geometric shape.
+
+        Raises:
+            Exception: if the area method is not implemented
+        """
+        raise Exception("area() is not implemented")
---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that defines a base geometry class"""
+
+
+class BaseGeometry:
+    """Defines methods for geometric shapes."""
+    pass
