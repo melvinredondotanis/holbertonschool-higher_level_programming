e5c6f63abd5a25aaea443816146864c1e4d84a91
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-09-25 12:11:52 +0200

    Refactoriser la fonction is_same_class pour utiliser l'opérateur "is" au lieu de la fonction isinstance

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -13,4 +13,4 @@ def is_same_class(obj, a_class):
     Returns:
         True if obj is exactly an instance of a_class, False otherwise
     """
-    return isinstance(type(obj), a_class)
+    return True if type(obj) is a_class else False
