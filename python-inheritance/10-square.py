aafa448153c1b86b531864feb93f623f64a28cd1
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-09-25 16:08:30 +0200

    Refactoriser les modules Square et Rectangle pour ajouter des commentaires de documentation

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Module that defines a Square class"""
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Module that defines a Square class"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
