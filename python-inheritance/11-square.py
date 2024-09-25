fb061d6c48393ba934e883c248606d83e2f334ef
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-09-25 15:35:25 +0200

    Refactoriser la classe Square pour supprimer le code inutile d'affichage du carré et réorganiser l'initialisation de l'attribut size

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Represents a square.
+    """
+    def __init__(self, size):
+        """
+        Initializes a Square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        Computes the area of a Square instance.
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """Return a string representation of the square."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module that defines a base geometry class"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Defines a rectangle."""
+
+    def __init__(self, width, height):
+        """Initializes a rectangle.
+
+        Args:
+            width (int): the width of the rectangle.
+            height (int): the height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Calculate the area of a rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return a string representation of the rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
