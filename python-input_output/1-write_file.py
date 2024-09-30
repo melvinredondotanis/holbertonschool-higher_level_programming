93cae9546211a1bb887996edf58b000d2ff930fe
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-09-30 14:14:08 +0200

    Refactoriser les scripts de manipulation de fichiers pour utiliser l'interpréteur Python par défaut

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def write_file(filename="", text=""):
-    """ Writes a text to a file (UTF8). """
+    """
+    Writes a text to a file (UTF8).
+    """
     with open(filename) as f:
         f.write(text)
--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def append_write(filename="", text=""):
-    """ Appends a text to a file (UTF8). """
+    """
+    Appends a text to a file (UTF8).
+    """
     with open(filename, 'a') as f:
         f.write(text)
