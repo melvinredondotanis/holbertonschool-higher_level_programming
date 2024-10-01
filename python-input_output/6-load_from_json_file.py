d09533490074410ef525657d61ec123dec7be8ba
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-10-01 12:15:53 +0200

    Refactoriser la fonction load_from_json_file pour utiliser la méthode json.load()

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,6 +6,7 @@ def load_from_json_file(filename):
     """
     return a json file
     """
+    import json
 
-    with open(filename, "r") as f:
-        return f
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
