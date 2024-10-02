6a201614af9328c8831063782ddb37cec0cf864f
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-10-02 14:34:58 +0200

    Refactoriser les méthodes de sérialisation pour utiliser l'encodage UTF-8 lors de l'ouverture des fichiers

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,13 +5,13 @@
 def serialize_and_save_to_file(data, filename):
     """tkt"""
     import pickle
-    with open(filename, "wb", encoding="utf-8") as f:
+    with open(filename, "wb") as f:
         f.write(pickle.dumps(data))
 
 
 def load_and_deserialize(filename):
     """tkt"""
     import pickle
-    with open(filename, "rb", encoding="utf-8") as f:
+    with open(filename, "rb") as f:
         data = pickle.loads(f.read())
     return data
