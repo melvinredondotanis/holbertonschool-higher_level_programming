5f966b273c8dc8fbc0c8dd16f684f84703d55e17
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-10-02 15:01:40 +0200

    Refactoriser les méthodes de sérialisation pour utiliser l'encodage UTF-8 lors de l'ouverture des fichiers

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Converts a class to a JSON string."""
+
+
+class CustomObject:
+    """Custom class to be serialized."""
+    def __init__(self, name, age, is_student):
+        """Initialize object attributes."""
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Display object attributes."""
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        """Serialize object to file."""
+        import pickle
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
+
+    @staticmethod
+    def deserialize(filename):
+        """Deserialize object from file."""
+        import pickle
+        with open(filename, "rb") as f:
+            return pickle.load(f)
