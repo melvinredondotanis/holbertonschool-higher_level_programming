46ad69eaf924032929380bb1fa16895dadeb0fb2
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-10-02 16:03:49 +0200

    Refactoriser les méthodes de sérialisation pour gérer les erreurs de fichier non trouvé lors de la conversion CSV vers JSON

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -10,10 +10,15 @@ def convert_csv_to_json(csv_file):
     import json
 
     data = []
-    with open(csv_file, "r") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            data.append(row)
+    try:
+        with open(csv_file, "r") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                data.append(row)
+    except FileNotFoundError:
+        print("file not found")
+        return False
 
     with open("data.json", "w") as f:
         f.write(json.dumps(data))
+    return True
