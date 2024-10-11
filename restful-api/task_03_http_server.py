5ecd0bc8ce117e034394b429930b5a6e04bdc8ad
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-10-11 11:44:13 +0200

    Refactoriser la gestion des erreurs de l'endpoint non trouvé dans le serveur HTTP

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -40,7 +40,7 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 
 if __name__ == "__main__":
