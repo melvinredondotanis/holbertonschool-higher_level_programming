b92749afbc07b0b111b79bb3f068b2b5f4d37df5
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-11-03 19:59:35 +0100

    Correction de l'affichage des villes pour afficher les noms au lieu des tuples dans le script de filtrage des villes

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -17,5 +17,4 @@ if __name__ == "__main__":
     cursor.execute("SELECT cities.id, cities.name, states.name \
     FROM cities JOIN states ON cities.state_id = states.id \
     WHERE states.name = '{}';".format(sys.argv[4]))
-    for state in cursor.fetchall():
-        print(state)
+    print(", ".join([state[1] for state in states]))
