ad1279231912af2e64ca3a2a2274364f047b299c
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-11-03 16:37:53 +0100

    Ajout d'une documentation pour le module et spécification du port de connexion à la base de données

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+Module that lists all states from the database hbtn_0e_0_usa
+"""
 import sys
 import MySQLdb
 
@@ -8,7 +10,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
+        port=3306
     )
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states")
