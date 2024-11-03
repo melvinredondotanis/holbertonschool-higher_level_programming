c25829b97f7bd767d4ce4171fdfbf6ea7be8e864
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-11-03 21:10:10 +0100

    Correction de la création de la session dans le script de suppression des états

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    session = sessionmaker(bind=engine)
-    session = sessionmaker(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
     states = session.query(State).filter(State.name.like('%a%')).all()
     for state in states:
         session.delete(state)
