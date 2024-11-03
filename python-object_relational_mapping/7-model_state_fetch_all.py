710be25dbef9f02a8f4d366a6943a57198eea92e
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-11-03 21:04:54 +0100

    Correction de la création de la session dans le script de récupération des états

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,7 +13,10 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    session = sessionmaker(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
     states = session.query(State).order_by(State.id).all()
+
     for state in states:
         print("{}: {}".format(state.id, state.name))
